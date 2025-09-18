a1a6247b6df79ca0196af3e54883d033f0e5201c
Vurdalak <Vurdalak@HOME-PC>
2025-09-18 20:10:52 +0300

    Добавьте файлы проекта.

---
+++ dz/dz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_CTYPE, "RUS");
+	// объ€вление констант
+	
+	const int hodba = 4;
+	const int beg = hodba * 3;
+	
+	// задание конкретного значени€ рассто€ни€(в км)
+	
+	float x = 36.0;
+
+	//расчет времени
+	
+	float vremya = x / beg;
+
+	// форматированный вывод результатов
+
+	printf("--–асчет времени, за которое мальчик преодолел рассто€ние--\n");
+	printf("\n===========================================================");
+	printf("\n                        ”—Ћќ¬»я: \n");
+	printf("");
+	printf("");
+	printf("");
+	printf("");
+	printf("");
+	printf("");
+	printf("");
+	printf("");
+
+
+
+}
